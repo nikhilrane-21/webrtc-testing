d108a064cad80e4f9760f09ccc1f4083bcbdfb57
Nikhil Rane <99159420+nikhilrane-21@users.noreply.github.com>
2023-09-18 09:54:41 +0530

    Add files via upload

---
+++ teete.py
@@ -0,0 +1,33 @@
+import cv2
+import streamlit as st
+import tempfile
+import base64
+
+
+def generate_frames(video):
+    while True:
+        success, frame = video.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        yield base64.b64encode(frame).decode()
+
+
+if __name__ == "__main__":
+    st.title("Video Player")
+
+    uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mkv", "mov"])
+
+    if uploaded_file is not None:
+        tfile = tempfile.NamedTemporaryFile(delete=False)
+        tfile.write(uploaded_file.read())
+
+        vf = cv2.VideoCapture(tfile.name)
+
+        for frame in generate_frames(vf):
+            # Display the frame in an HTML img element
+            st.markdown(f'<img src="data:image/jpeg;base64,{frame}"/>', unsafe_allow_html=True)
+
+        vf.release()
